ada31a95e708c842d59b05c31cfc62b10a0592f4
Mayank Chauhan <mayankdarbar907@gmail.com>
2025-04-14 10:54:39 +0530

    Update MiniMap dimensions and implement fullscreen map functionality with toggle buttons

--- Assets/Scripts/MiniMapController.cs
+++ Assets/Scripts/MiniMapController.cs
@@ -1,23 +1,39 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniMapController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private Transform miniMapCamTransform;
+
+    [SerializeField] private GameObject fullscreenMap;
+    [SerializeField] private Button openMapBtn, closeMapBtn;
+
+    void Start()
+    {
+        fullscreenMap.SetActive(false); // Ensure the fullscreen map is initially inactive
+
+        openMapBtn.onClick.AddListener(OpenMap); // Add listener to open map button
+        closeMapBtn.onClick.AddListener(CloseMap); // Add listener to close map button
+    }
 
     void LateUpdate()
     {
         Vector3 newLocation = player.position;
-        newLocation.y = transform.position.y; // Keep the y position of the minimap constant
-        transform.position = newLocation; // Update the minimap position to follow the player
+        newLocation.y = miniMapCamTransform.transform.position.y; // Keep the y position of the minimap constant
+        miniMapCamTransform.transform.position = newLocation; // Update the minimap position to follow the player
 
         // transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0); // Rotate the minimap to match the player's rotation
     }
 
-    public void OpenMap()
+    private void OpenMap()
     {
-        // Open the map UI
-        // Implement your logic to open the map here
-        Debug.Log("Map opened");
+        fullscreenMap.SetActive(true); // Activate the fullscreen map
+    }
+
+    private void CloseMap()
+    {
+        fullscreenMap.SetActive(false); // Deactivate the fullscreen map
     }
 }
